b1c9f771ca6b9a7471540e8a1b07cfaf628c126a
Developer Tested <89337605+developerTested@users.noreply.github.com>
2024-10-27 02:23:27 +0530

    Emoji Picker done

--- frontend/src/components/Emoji/index.tsx
+++ frontend/src/components/Emoji/index.tsx
@@ -5,15 +5,16 @@ import { categories } from '@/utilities/menu';
 
 type EmojiPickerProps = {
     enableSearch?: boolean,
+    onEmojiClick: (emojiData: EmojiType) => void,
 }
 
 type GroupList = {
     id: string,
     name: string,
-    emoji: emoji[],
+    emoji: EmojiType[],
 }
 
-type emoji = {
+export type EmojiType = {
     codes: string;
     char: string;
     name: string;
@@ -23,11 +24,17 @@ type emoji = {
 }
 
 
-export default function EmojiPicker({ enableSearch = true }: EmojiPickerProps) {
+export default function EmojiPicker({ enableSearch = true, onEmojiClick }: EmojiPickerProps) {
 
     const [tab, setTab] = useState('smile');
     const [filteredEmojiList, setFilteredEmojiList] = useState<GroupList[]>([]);
-    const [mostUsed, setMostUsed] = useState([]);
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const [mostUsed, setMostUsed] = useState<EmojiType[]>(() => {
+        const savedMostUsed = localStorage.getItem('mostUsedEmojis');
+        return savedMostUsed ? JSON.parse(savedMostUsed) : [];
+    });
 
     const searchRef = useRef(null);
     const emojiPickerRef = useRef<HTMLDivElement | null>(null);
@@ -56,7 +63,6 @@ export default function EmojiPicker({ enableSearch = true }: EmojiPickerProps) {
             const emojiPicker = emojiPickerRef.current;
             const scrollToPosition = groupRef.offsetTop - emojiPicker.offsetTop;
 
-            // Ensure the scroll operation is only performed if emojiPicker is available
             emojiPicker.scrollTo({
                 top: scrollToPosition,
                 behavior: 'smooth',
@@ -64,9 +70,27 @@ export default function EmojiPicker({ enableSearch = true }: EmojiPickerProps) {
         }
     };
 
-    const handleEmojiClick = (emoji: emoji) => {
-       
+    const handleEmojiClick = (emoji: EmojiType) => {
+        const updatedMostUsed = [...mostUsed];
+
+        const index = updatedMostUsed.findIndex((e: EmojiType) => e.char === emoji.char);
+
+        onEmojiClick(emoji)
+
+        if (index !== -1) {
+            updatedMostUsed.splice(index, 1); // Remove it to re-add later
+        }
+
+        updatedMostUsed.unshift(emoji);
+
+        if (updatedMostUsed.length > 10) {
+            updatedMostUsed.pop();
+        }
+
+        setMostUsed(updatedMostUsed);
+        localStorage.setItem('mostUsedEmojis', JSON.stringify(updatedMostUsed));
     };
+
 
     const loadEmojis = () => {
 
@@ -92,28 +116,45 @@ export default function EmojiPicker({ enableSearch = true }: EmojiPickerProps) {
         loadEmojis();
     }, []);
 
+
+    const filteredEmojiGroups = filteredEmojiList.map(group => ({
+        ...group,
+        emoji: group.emoji.filter(emoji => emoji.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    })).filter(group => group.emoji.length > 0);
+
     return (
-        <div className='emoji-picker w-350 h-350 z-10 absolute top-full left-auto right-auto shadow-lg bg-white dark:bg-widget rounded-lg dark:text-white p-2 border border-black flex flex-col gap-4'>
-            <div className="flex items-center">
+        <div className='emoji-picker grid w-350 h-350 z-10 absolute top-full left-auto right-auto shadow-lg bg-white dark:bg-widget rounded-lg dark:text-white border border-black'>
+            <div className="flex items-center p-2">
                 {categories.map((cat) => <button onClick={() => handleTabClick(cat.id)} key={cat.name} className={`border-b-2 ${tab === cat.id ? "border-blue-500" : "border-transparent"} py-2 flex-auto flex items-center justify-center`}>
                     {cat.icon}
                 </button>)}
             </div>
 
+            <div className="block most-used p-2">
             {enableSearch ?
-                <Input ref={searchRef} type="search" name="search" placeholder='Connecting People...' className='bg-slate-100 dark:bg-input border-white dark:border-input dark:text-white border-none px-4 p-2.5 outline-none' />
+                <Input ref={searchRef} onChange={(e) => setSearchQuery(e.target.value)} type="search" name="search" placeholder='Connecting People...' className='bg-slate-100 dark:bg-input border-white dark:border-input dark:text-white border-none px-4 p-2.5 outline-none' />
                 : ""}
 
-            <div ref={emojiPickerRef} onScroll={handleOnScroll} className="size-full overflow-y-auto flex flex-col gap-4">
-                {filteredEmojiList.map((group, groupIndex) => (
-                    <div key={groupIndex} className="block" ref={el => (groupRefs.current[groupIndex] = el)}>
-                        <div className="text-sm my-2">
+                <div className="text-sm font-semibold my-2">Most Used</div>
+                <div className="flex flex-wrap items-center text-2xl">
+                    {mostUsed.map((emoji, emojiIndex) => (
+                        <div key={emojiIndex} className="flex-auto flex items-center justify-center hover:bg-slate-200 rounded-full cursor-pointer" onClick={() => handleEmojiClick(emoji)}>
+                            {emoji.char}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div ref={emojiPickerRef} onScroll={handleOnScroll} className="size-full overflow-y-auto space-y-4">
+                {filteredEmojiGroups.map((group, groupIndex) => (
+                    <div key={groupIndex} className="block p-2 space-y-2" ref={el => (groupRefs.current[groupIndex] = el)}>
+                        <div className="text-sm font-semibold">
                             {group.name}
                         </div>
 
-                        <div className="flex flex-wrap items-center text-2xl">
+                        <div className="flex flex-wrap text-2xl">
                             {group.emoji.map((emoji, emojiIndex) => (
-                                <div key={emojiIndex} className="flex-auto flex items-center justify-center hover:bg-slate-200 rounded-full cursor-pointer">
+                                <div key={emojiIndex} onClick={() => handleEmojiClick(emoji)} className="w-8 h-8 hover:bg-slate-200 rounded-full cursor-pointer">
                                     {emoji.char}
                                 </div>
                             ))}
